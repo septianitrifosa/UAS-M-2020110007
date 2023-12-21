7624482d189ff3b7cfb3c98d16e00a23c12e4cd5
septiani TD <bhogaster@gmail.com>
2023-12-21 19:40:43 +0700

    Soal 1

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('account', AccountController::class);
+Route::resource('transaction', TransactionController::class);
+
